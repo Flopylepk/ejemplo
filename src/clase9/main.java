a0ac54f8559761efaf385a9d0d96f7ef3f5059ac
Flopylepk <florencia.torres@davinci.edu.ar>
2024-09-09 10:11:15 -0300

    agrego saludo

--- src/clase9/main.java
+++ src/clase9/main.java
@@ -1,10 +1,13 @@
 package clase9;
 
+import javax.swing.JOptionPane;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		JOptionPane.showMessageDialog(null, "hola mundo");
 	}
 
 }
